9723a77d47cac63feafa2934576bc978f0ca2b0b
czulueta <https://github.com/czulueta/weed.git>
2020-12-24 22:58:23 -0800

    setup basic server file on the backend

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use(9000, () => {
+    console.log("successfully running on port 9000")
+})
